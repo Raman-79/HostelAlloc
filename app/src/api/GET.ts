bbc110f5eca3e70fbde4eeeea8e4e16c979e5fb5
unknown <ramansinghdsat@gmail.com>
2024-07-25 16:18:40 +0530

    Changes when connecting to mobile phone in API

--- app/src/api/GET.ts
+++ app/src/api/GET.ts
@@ -2,3 +2,5 @@ export const GET_STUDENT = 'http://10.0.2.2:3000/api/v1/student/user'
 export const SEARCH_API  =  'http://10.0.2.2:3000/api/v1/student/find'
 export const GET_HOSTELS = 'http://10.0.2.2:3000/api/v1/hostel/all'
 export const LOGIN = 'http://10.0.2.2:3000/api/v1/user/signin'
+
+//Replace 10.0.2.2 with 192.168.0.101 to run it on Sam Laptop... or find IPv4 address by ipconfig in terminal after connecting laptop to phone by USB
